022fb63318943c37e931f540856392314c46f3d3
Lanvige Jiang <lanvige@gmail.com>
2014-12-25 12:06:39 +0800

    update main js

--- js/main.js
+++ js/main.js
@@ -43,56 +43,6 @@ BRUSHED.listenerMenu = function(){
 
 
 /* ==================================================
-   Slider Options
-================================================== */
-
-BRUSHED.slider = function(){
-    $.supersized({
-        // Functionality
-        slideshow               :   1,          // Slideshow on/off
-        autoplay                :   1,          // Slideshow starts playing automatically
-        start_slide             :   1,          // Start slide (0 is random)
-        stop_loop               :   0,          // Pauses slideshow on last slide
-        random                  :   0,          // Randomize slide order (Ignores start slide)
-        slide_interval          :   12000,      // Length between transitions
-        transition              :   1,          // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-        transition_speed        :   300,        // Speed of transition
-        new_window              :   1,          // Image links open in new window/tab
-        pause_hover             :   0,          // Pause slideshow on hover
-        keyboard_nav            :   1,          // Keyboard navigation on/off
-        performance             :   1,          // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-        image_protect           :   1,          // Disables image dragging and right click with Javascript
-                                                   
-        // Size & Position                         
-        min_width               :   0,          // Min width allowed (in pixels)
-        min_height              :   0,          // Min height allowed (in pixels)
-        vertical_center         :   1,          // Vertically center background
-        horizontal_center       :   1,          // Horizontally center background
-        fit_always              :   0,          // Image will never exceed browser width or height (Ignores min. dimensions)
-        fit_portrait            :   1,          // Portrait images will not exceed browser height
-        fit_landscape           :   0,          // Landscape images will not exceed browser width
-                                                   
-        // Components                           
-        slide_links             :   'blank',    // Individual links for each slide (Options: false, 'num', 'name', 'blank')
-        thumb_links             :   0,          // Individual thumb links for each slide
-        thumbnail_navigation    :   0,          // Thumbnail navigation
-        slides                  :   [           // Slideshow Images
-                                            {image : 'img/slider-images/image01.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-                                            {image : 'img/slider-images/image02.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-                                            {image : 'img/slider-images/image03.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-                                            {image : 'img/slider-images/image04.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
-                                    ],
-                                    
-        // Theme Options               
-        progress_bar            :   0,          // Timer for each slide                         
-        mouse_scrub             :   0
-        
-    });
-
-}
-
-
-/* ==================================================
    Navigation Fix
 ================================================== */
 
@@ -113,10 +63,7 @@ BRUSHED.filter = function (){
           // options
           animationEngine: 'best-available',
           itemSelector : '.item-thumbs',
-          layoutMode : 'fitRows',
-          masonry: {
-            columnWidth: 1000
-          }
+          layoutMode : 'fitRows'
         });
         
         // filter items when filter link is clicked
@@ -212,36 +159,6 @@ BRUSHED.contactForm = function(){
     });
 }
 
-
-/* ==================================================
-   Twitter Feed
-================================================== */
-
-BRUSHED.tweetFeed = function(){
-    var valueTop = -64;
-    
-    $("#ticker").tweet({
-          username: "Bluxart",
-          page: 1,
-          avatar_size: 0,
-          count: 10,
-          template: "{text}{time}",
-          filter: function(t){ return ! /^@\w+/.test(t.tweet_raw_text); },
-          loading_text: "loading ..."
-    }).bind("loaded", function() {
-      var ul = $(this).find(".tweet_list");
-      var ticker = function() {
-        setTimeout(function() {
-            ul.find('li:first').animate( {marginTop: valueTop + 'px'}, 500, 'linear', function() {
-                $(this).detach().appendTo(ul).removeAttr('style');
-            }); 
-          ticker();
-        }, 5000);
-      };
-      ticker();
-    });
-    
-}
 
 
 /* ==================================================
@@ -397,8 +314,6 @@ BRUSHED.toolTip = function(){
     Init
 ================================================== */
 
-BRUSHED.slider();
-
 $(document).ready(function(){
     Modernizr.load([
     {
@@ -435,9 +350,9 @@ $(document).ready(function(){
     BRUSHED.goSection();
     BRUSHED.goUp();
     BRUSHED.filter();
+    $('#projects').animate({opacity: 1.0}, 200);
     BRUSHED.fancyBox();
     BRUSHED.contactForm();
-    BRUSHED.tweetFeed();
     BRUSHED.scrollToTop();
     BRUSHED.utils();
     BRUSHED.accordion();
